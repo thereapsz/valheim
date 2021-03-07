b93a6b4b1f38ec77a3567d2db4b907832b5e2a23
Runar Klemetsdal <reapsz@outlook.com>
2021-03-07 13:44:10 +0100

    commit

--- start.sh
+++ start.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 echo "${SERVER_NAME} with world: ${WORLD} savedir: $SAVE_DIR"
 
+
 #check if auto update is enabled
+echo "AutoUpdate Check"
 if [ "${AUTOUPDATE}" == "true" ]; then
   echo "Update True"
-    bash /steamcmd/steamcmd.sh +login anonymous +force_install_dir /valheim +app_update 896660 validate +quit
+  bash /steamcmd/steamcmd.sh +login anonymous +force_install_dir /valheim +app_update 896660 validate +quit
 fi
 
 #check if game is installed
+echo "already installed check"
 if [[ -f "start_server.sh" ]]; then
   echo "dir not empty, skipping..."
 else
-  echo "Dir is Empty"
+  echo "Dir is Empty, starting install"
   bash /steamcmd/steamcmd.sh +login anonymous +force_install_dir /valheim +app_update 896660 validate +quit
 fi
 
 #start game server
+echo "startserver check"
 if [[ -f "start_server.sh" ]]; then
-  echo "dir not empty"
+  echo "start files found, trying to start server"
   #copy from valheim start script
   export templdpath=$LD_LIBRARY_PATH
   export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
